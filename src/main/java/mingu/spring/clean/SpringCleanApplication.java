668b5ed53f13498a83c51d43b546ddc36620025b
jaden.song <jaden.song@kakaoenterprise.com>
2023-12-08 10:28:29 +0900

    init

---
+++ src/main/java/mingu/spring/clean/SpringCleanApplication.java
@@ -0,0 +1,13 @@
+package mingu.spring.clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCleanApplication.class, args);
+    }
+
+}
